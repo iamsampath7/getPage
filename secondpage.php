583ab5a3179c5c75cf3709c54d2dc66edb702c22
iamsampath7 <38104301+iamsampath7@users.noreply.github.com>
2018-07-03 20:29:14 +0530

    Update secondpage.php

--- secondpage.php
+++ secondpage.php
@@ -1,6 +1,11 @@
-<?php
+<?php 
+if(isset($_GET['userName']) And ($_GET['password'])) {
 $user=$_GET["userName"];
 $pass=$_GET["password"];
-
-echo "Welcome Mr.".$user;
+  if($user=="iamsampath420@gmail.com") And ($pass=="Sampath$1008")) {
+    echo "Welcome Mr.".$user;
+  } else {
+   eDcho "Sry U are Not Sampath.S!"; 
+  }
+}
 ?>
